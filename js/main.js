69c3af68e87c45abe721ddb4d245c288db655a38
spiredigitalnz <amit@spiredigital.co.nz>
2018-09-12 15:00:47 +1200

    UI almost finished for first page except date and checkbox

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+// Load Dom before running Jquery
+$(function(){
 
- $('.input-group.date').datepicker({format: "dd.mm.yyyy"}); 
+  // Variables
+  var icons = $('.icons');
+  var basicInfoButton = $('.basic-info-next-button');
+  var namePattern = new RegExp("^[a-zA-Z' .-]{1,50}$");
+  var emailPattern = new RegExp ("^[a-zA-Z0-9._-]*[@][a-zA-Z]*\.[a-zA-Z]{2,6}$");
+
+  icons.hide();
+
+  basicInfoButton.on('click',show);
+
+  function show(e){
+    e.preventDefault();
+    icons.toggle();
+  }
+
+
+
+});

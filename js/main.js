087fad904dda4082c16a6d6d638543c940df478b
spiredigitalnz <amit@spiredigital.co.nz>
2018-09-12 16:01:47 +1200

    form validation for name and email functionality is done

--- js/main.js
+++ js/main.js
@@ -6,14 +6,44 @@ $(function(){
   var basicInfoButton = $('.basic-info-next-button');
   var namePattern = new RegExp("^[a-zA-Z' .-]{1,50}$");
   var emailPattern = new RegExp ("^[a-zA-Z0-9._-]*[@][a-zA-Z]*\.[a-zA-Z]{2,6}$");
+  var totalErrors = 0;
+  var studentName = $('#student-name');
+  var studentEmail = $('#student-email');
+
+  // var value = studentName.val();
 
   icons.hide();
+
+
+  // Validation
+  studentName.on('keyup',validation );
+  studentName.on('blur',validation );
+  studentEmail.on('keyup',emailValidation );
+  studentEmail.on('blur',emailValidation );
+
+  function validation(){
+    // console.log(this.value);
+    if (namePattern.test(this.value)) {
+      $('.ok-icon').show();
+      $('.error-icon').hide();
+    }else{
+      $('.error-icon').show();
+    }
+  }
+  function emailValidation(){
+    // console.log(this.value);
+    if (emailPattern.test(this.value)) {
+      $('.email-ok-icon').show();
+      $('.email-error-icon').hide();
+    }else{
+      $('.email-error-icon').show();
+    }
+  }
 
   basicInfoButton.on('click',show);
 
   function show(e){
     e.preventDefault();
-    icons.toggle();
   }
 
 
